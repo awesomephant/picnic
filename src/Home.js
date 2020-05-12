d815efb486a1dc67c179674b388600736f287f98
Max Kohler <max@koehler-kn.de>
2020-05-12 22:37:21 +0100

    Add calendar toggle

--- src/Home.js
+++ src/Home.js
@@ -10,12 +10,19 @@ import {
 
 export default function Home(props) {
     const [calendarActive, setCalendarActive] = useState(false);
+    function toggleCalendar(){
+        if (calendarActive === true){
+            setCalendarActive(false)
+        } else {
+            setCalendarActive(true)
+        }
+    }
     return (
         <main>
             <nav className='site-nav'>
                 <Link className='highlight' to="/artist/">R1</Link>
                 <Link to="/editor/">Ready Meals</Link>
-                <a href="#1"><CurrentDate></CurrentDate></a>
+                <a href="#1" onClick={toggleCalendar}><CurrentDate></CurrentDate></a>
                 <a href="#1">Collective</a>
             </nav>
             <h1 className='site-title'>
@@ -23,9 +30,11 @@ export default function Home(props) {
         <img src={logo} alt='logo'></img>
             </h1>
             <Cloth></Cloth>
-            <Window title='Calendar' x={200} y={400} className='window' draggable={false}>
-                <Calendar></Calendar>
-        </Window>
+            {calendarActive === true &&
+                <Window title='Calendar' x={200} y={400} className='window' draggable={false}>
+                    <Calendar></Calendar>
+                </Window>
+            }
         </main>
     )
 }
